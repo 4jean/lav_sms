4de82a9fab3c16d029326b3b9d18367795d03ae3
Chinedu Okemiri <jokemiri@gmail.com>
2020-10-21 15:45:36 +0100

    Created Seeder for Many Students

--- database/migrations/2018_09_22_073005_create_my_classes_table.php
+++ database/migrations/2018_09_22_073005_create_my_classes_table.php
@@ -16,7 +16,7 @@ class CreateMyClassesTable extends Migration
         Schema::create('my_classes', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name', 100);
-            $table->unsignedInteger('class_type_id');
+            $table->unsignedInteger('class_type_id')->nullable();
             $table->timestamps();
         });
 
