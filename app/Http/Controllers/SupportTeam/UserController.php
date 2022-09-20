10a2e962686da88bd7bc20135c75032367cc0750
Chinedu Okemiri <jokemiri@gmail.com>
2022-09-20 21:09:01 +0100

    Fixed Improper Privileged Management

--- app/Http/Controllers/SupportTeam/UserController.php
+++ app/Http/Controllers/SupportTeam/UserController.php
@@ -126,6 +126,7 @@ class UserController extends Controller
 
         $data = $req->except(Qs::getStaffRecord());
         $data['name'] = ucwords($req->name);
+        $data['user_type'] = $user_type;
 
         if($user_is_staff && !$user_is_teamSA){
             $data['username'] = Qs::getAppCode().'/STAFF/'.date('Y/m', strtotime($req->emp_date)).'/'.mt_rand(1000, 9999);
