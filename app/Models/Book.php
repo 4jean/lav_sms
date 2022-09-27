55a02157103d6f62a796f7a7068f219f5624d868
Dominique Youness <youness.dominique@gmail.com>
2022-09-27 12:14:45 +0200

    [FEAT] Book model adding proteted fillable

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'my_class_id',
+        'description',
+        'author',
+        'book_type',
+        'url',
+        'location',
+        'total_copies',
+        'issued_copies'
+    ];
 }
